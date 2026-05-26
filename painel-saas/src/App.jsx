f95de38bd5714bdb649aa9372cd3663e24bed723
Renato Rocha <rsrconsultoriaevenda@gmail.com>
2026-05-26 12:32:12 -0300

    config: força pasta dist e atualiza rotas do painel

--- painel-saas/src/App.jsx
+++ painel-saas/src/App.jsx
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿import React, { useState, useEffect } from 'react';
+﻿﻿import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import {
